d233e37b45c17d98b6d5a5f2e0c69314c7632cc7
Bas950 <me@bas950.com>
2023-01-13 12:48:33 +0100

    chore(index): fix jira commitlint import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	eslint: require("../configs/.eslintrc.js"),
 	commitlint: require("../configs/.commitlintrc.js"),
-	commitlintJira: require("../configs/jira.commitlint.js"),
+	commitlintJira: require("../configs/jira.commitlintrc.js"),
 	lintstaged: require("../configs/.lintstagedrc.js"),
 	prettier: require("../configs/.prettierrc.js"),
 };
